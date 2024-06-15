d73e773d6fa54ec216b8b16c632a993cbe239635
SangWon7242 <gotkdqja@gmail.com>
2024-06-15 12:38:02 +0900

    컨트롤러 연습, no값 증가, 감소, 초기화 기능 구현

--- src/main/java/com/sbs/basic2/HelloController.java
+++ src/main/java/com/sbs/basic2/HelloController.java
@@ -6,9 +6,35 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
+  int no = 0;
+
   @GetMapping("/usr/hello")
   @ResponseBody
   public String Hello() {
     return "Hello World";
+  }
+
+  @GetMapping("/usr/increase")
+  @ResponseBody
+  public int increaseNo() {
+    no++;
+
+    return no;
+  }
+
+  @GetMapping("/usr/decrease")
+  @ResponseBody
+  public int decreaseNo() {
+    no--;
+
+    return no;
+  }
+
+  @GetMapping("/usr/reset")
+  @ResponseBody
+  public int resetNo() {
+    no = 0;
+
+    return no;
   }
 }
